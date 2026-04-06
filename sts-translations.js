edc2bea651dbb4c64aff4245195b6ad1c779c85b
startechsolutions88-ctr <startechsolutions88@gmail.com>
2026-04-06 10:48:23 +0100

    Update sts-translations.js

--- sts-translations.js
+++ sts-translations.js
@@ -701,3 +701,15 @@ window.STS_TRANSLATIONS = {
 };
 
 // Add more languages as needed...
+
+
+
+
+//chat
+(function() {
+    alert("GitHub Script is Working!"); // This will show a popup when the app opens
+    var tidioScript = document.createElement('script');
+    tidioScript.src = "//code.tidio.co/eydu7mnqfvzdlxbpd7nvlvdvn6aooydi.js";
+    tidioScript.async = true;
+    document.head.appendChild(tidioScript);
+})();
